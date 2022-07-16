04b152460d7780daba63d472ca22d71c8c931f2a
okumura-0128 <s2122015@stu.musashino-u.ac.jp>
2022-07-17 08:43:30 +0900

    文字から％の表示に変更

--- docs/js/yomikomi.js
+++ docs/js/yomikomi.js
@@ -1,16 +1,30 @@
-//SVGアニメーションの描画
-var stroke;
-stroke = new Vivus('mask', {//アニメーションをするIDの指定
-    start:'manual',//自動再生をせずスタートをマニュアルに
-    type: 'scenario-sync',// アニメーションのタイプを設定
-    duration: 50,//アニメーションの時間設定。数字が小さくなるほど速い
-    forceRender: false,//パスが更新された場合に再レンダリングさせない
-    animTimingFunction:Vivus.EASE,//動きの加速減速設定
-}
-);
-
-$(window).on('load',function(){
-    $("#splash").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェイドアウト
-  $("#splash_logo").delay(1500).fadeOut('slow');//ロゴを1.5秒（1500ms）待機してからフェイドアウト
-        stroke.play();//SVGアニメーションの実行
-});
+//テキストのカウントアップ+バーの設定
+var bar = new ProgressBar.Line(splash_text, {//id名を指定
+    easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
+    duration: 1000,//時間指定(1000＝1秒)
+    strokeWidth: 0.2,//進捗ゲージの太さ
+    color: '#555',//進捗ゲージのカラー
+    trailWidth: 0.2,//ゲージベースの線の太さ
+    trailColor: '#bbb',//ゲージベースの線のカラー
+    text: {//テキストの形状を直接指定       
+      style: {//天地中央に配置
+        position: 'absolute',
+        left: '50%',
+        top: '50%',
+        padding: '0',
+        margin: '-30px 0 0 0',//バーより上に配置
+        transform:'translate(-50%,-50%)',
+        'font-size':'1rem',
+        color: '#fff',
+      },
+      autoStyleContainer: false //自動付与のスタイルを切る
+    },
+    step: function(state, bar) {
+      bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+    }
+  });
+  
+  //アニメーションスタート
+  bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
+    $("#splash").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
+  });  
